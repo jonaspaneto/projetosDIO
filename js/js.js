b01f0907c53f93ebebe80e5e3a116264cfd852cb
Jonas <jonaspaneto@gmail.com>
2021-07-18 17:43:11 -0300

    Criação dos movimentos do inimigo 1

--- js/js.js
+++ js/js.js
@@ -11,6 +11,8 @@ function start() {
   //Principais variáveis do jogo
 
   var jogo = {};
+  var velocidade = 5;
+  var posicaoY = parseInt(Math.random() * 334);
   var TECLA = {
     W: 87,
     S: 83,
@@ -36,6 +38,7 @@ function start() {
   function loop() {
     movefundo();
     movejogador();
+    moveinimigo1();
   } // Fim da função loop()
 
   //Função que movimenta o fundo do jogo
@@ -66,5 +69,17 @@ function start() {
       //Chama função Disparo
     }
   } // fim da função movejogador()
+
+  function moveinimigo1() {
+    posicaoX = parseInt($("#inimigo1").css("left"));
+    $("#inimigo1").css("left", posicaoX - velocidade);
+    $("#inimigo1").css("top", posicaoY);
+
+    if (posicaoX <= 0) {
+      posicaoY = parseInt(Math.random() * 334);
+      $("#inimigo1").css("left", 694);
+      $("#inimigo1").css("top", posicaoY);
+    }
+  } //Fim da função moveinimigo1()
 } // Fim da função start
 
